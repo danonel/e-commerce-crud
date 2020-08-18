3e02b1f990f36879f7494a2ab72b6ace916a1f87
Daniel Luis <56178612+danonel@users.noreply.github.com>
2020-08-18 01:29:38 -0300

    Add: check same title in products

--- src/routes/products.routes.ts
+++ src/routes/products.routes.ts
@@ -1,18 +1,33 @@
 import { Router } from 'express';
-import { uuid } from 'uuidv4'
+import { uuid } from 'uuidv4';
 
 const productsRouter = Router()
 
-const products = []
+interface Product {
+  id: string,
+  title: string,
+  amout: number,
+  price: number,
+}
+
+const products: Product[] = []
 
 productsRouter.post('/', (req, res) => {
-  const { title, amout, price } = req.body
+  const { title, amout, price } = req.body;
+
+  const checkSameTitle = products.find(product =>
+    product.title
+  )
+
+  if (checkSameTitle) {
+    return res.status(400).json({ message: 'product with same title' })
+  }
 
   const product = {
     id: uuid(),
     title,
     amout,
-    price
+    price,
   }
 
   products.push(product)
